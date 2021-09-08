04a3db545ba865be7511dbf69eb2ba6165bd4a4f
RodrigoTomeES <rotome@unirioja.es>
2021-09-08 13:20:31 +0200

    Added support to transform all EML files from a directory

--- parse.js
+++ parse.js
@@ -1,26 +1,54 @@
 #!/usr/bin/env node
 
 import { MailParser } from 'mailparser';
-import { writeFileSync, createReadStream } from 'fs';
-import nomnom from 'nomnom'
+import { writeFileSync, createReadStream, readdir } from 'fs';
+import { basename } from 'path';
+import nomnom from 'nomnom';
 
-const mailparser = new MailParser();
-const opts = nomnom.option('infile', {
+const opts = nomnom
+  .option('directory', {
+    abbr: 'd',
+    flag: true,
+    help: 'Adds the ability to use directories instead of files'
+  })
+  .option('infile', {
     abbr: 'i',
     required: true,
-    help: 'Specify input file'
+    help: 'Specify input file or directory with -d flag'
   })
   .option('outfile', {
     abbr: 'o',
     required: true,
-    help: 'Specify output file'
+    help: 'Specify output file or directory with -d flag'
   })
   .parse();
 
-mailparser.on('data', data => {
-  if (data.type === 'text') {
-    writeFileSync(opts.outfile, data.html);
-  }
-});
+if (opts.directory) {
+  readdir(opts.infile, (err, files) => {
+    if (err) console.error("Error:", err);
+    else {
+      files.forEach(file => {
+        const fileName = basename(file, '.eml');
+        const mailparser = new MailParser();
+  
+        mailparser.on('data', data => {
+          if (data.type === 'text') {
+            writeFileSync(`${opts.outfile}/${fileName}.html`, data.html);
+          }
+        });
+  
+        createReadStream(`${opts.infile}/${file}`).pipe(mailparser);
+      });
+    }
+  });
+} else {
+  const mailparser = new MailParser();
 
-createReadStream(opts.infile).pipe(mailparser);
+  mailparser.on('data', data => {
+    if (data.type === 'text') {
+      writeFileSync(opts.outfile, data.html);
+    }
+  });
+
+  createReadStream(opts.infile).pipe(mailparser);
+}
